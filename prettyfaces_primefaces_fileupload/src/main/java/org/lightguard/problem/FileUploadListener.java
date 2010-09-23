f384ae39ad7d7767e97ba0e85fec306cc0d07661
LightGuard <lightguard.jp@gmail.com>
2010-09-23 08:09:24 -0600

    Not seeing the exception, but here's the war

--- prettyfaces_primefaces_fileupload/src/main/java/org/lightguard/problem/FileUploadListener.java
+++ prettyfaces_primefaces_fileupload/src/main/java/org/lightguard/problem/FileUploadListener.java
@@ -2,14 +2,16 @@ package org.lightguard.problem;
 
 import org.primefaces.event.FileUploadEvent;
 
-import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import java.io.Serializable;
 
-@Model
-public class FileUploadListener {
-    public void onFileUpload(FileUploadEvent event)
-    {
+@ManagedBean
+@ViewScoped
+public class FileUploadListener implements Serializable {
+    public void onFileUpload(FileUploadEvent event) {
         FacesMessage msg = new FacesMessage(String.format("File '%s' uploaded", event.getFile().getFileName()));
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
